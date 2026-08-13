e0b1238cc79d67dfc1e18716ec44a67258f475fa
AAtaksan <arpittaksande@gmail.com>
2026-08-13 13:42:45 +0530

    Engine / Session

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
-from sqlalchemy import create_engine, select
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, Session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase, Session
 
 DATABASE_URL = "sqlite:///./expenses.db"
 
@@ -7,18 +7,6 @@ engine = create_engine(DATABASE_URL) # engine is the main entry point for SQLAlc
 
 class Base(DeclarativeBase): # Classes that inherit from this base are going to describe database tables
     pass
-
-class Expense(Base):
-    __tablename__ = "expenses" # tell pyhton -> Expense Python class represents the expenses database table.
-
-    id: Mapped[int] = mapped_column(primary_key=True) # id is a SQLAlchemy-mapped attribute whose Python type is int. and mapped_column(...) defines how that attribute becomes a database column.
-    name: Mapped[str]
-    amount: Mapped[float]
-    category: Mapped[str]
-    date: Mapped[str]
-    payment: Mapped[str]
-
-Base.metadata.create_all(engine)
 
 def get_session():
     with Session(engine) as session:
